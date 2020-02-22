4ac1d99bcb62b24a58cd96a545a2890a3e386974
kibuna <kibuna0410@gmail.com>
2020-02-22 11:58:29 +0900

    updated edgesin snippets

--- snippets/edgesin.cpp
+++ snippets/edgesin.cpp
@@ -2,7 +2,7 @@ vector<vector<int>> edges(n);
 for (int i = 0; i < ${1:n-1}; ++i) {
     int u, v;
     cin >> u >> v;
-    // u--, v--;
+    u--, v--;
     edges[u].emplace_back(v);
     edges[v].emplace_back(u);
 }
