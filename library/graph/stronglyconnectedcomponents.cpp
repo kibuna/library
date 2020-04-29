ecbecf5163bd140489a1b8b8d65d2a2f2924ccb6
kibuna <kibuna0410@gmail.com>
2020-04-29 10:16:05 +0900

    updated ssc, lca

--- library/graph/stronglyconnectedcomponents.cpp
+++ library/graph/stronglyconnectedcomponents.cpp
@@ -1,16 +1,18 @@
 struct StronglyConnectedComponents {
     vector<vector<int>> const &edges_in;
-    vector<vector<int>> edges, rev;
+    vector<vector<int>> edges, rev, dag;
     vector<int> comp, order, used;
+    vector<vector<int>> cs; // list of nodes in each component
 
     StronglyConnectedComponents(vector<vector<int>> const &g)
-        : edges_in(g), edges(g.size()), rev(g.size()), comp(g.size(), -1), used(g.size()) {
+        : edges_in(g), edges(g.size()), rev(g.size()), comp(g.size(), -1), used(g.size()), cs(g.size()) {
         for (int i = 0; i < (int)edges_in.size(); ++i) {
             for (auto &v : edges_in[i]) {
                 edges[i].push_back(v);
                 rev[v].push_back(i);
             }
         }
+        build();
     }
     int operator[](int k) { return comp[k]; }
     void dfs(int c) {
@@ -25,10 +27,11 @@ struct StronglyConnectedComponents {
         if (comp[c] != -1)
             return;
         comp[c] = cnt;
+        cs[cnt].emplace_back(c);
         for (auto &v : rev[c])
             rdfs(v, cnt);
     }
-    vector<vector<int>> build() {
+    void build() {
         for (int i = 0; i < (int)edges.size(); ++i)
             dfs(i);
         reverse(order.begin(), order.end());
@@ -37,16 +40,15 @@ struct StronglyConnectedComponents {
             if (comp[i] == -1)
                 rdfs(i, ptr++);
         }
-        vector<vector<int>> ret(ptr);
+        dag.resize(ptr);
         for (int i = 0; i < (int)edges_in.size(); ++i) {
             int x = comp[i];
             for (auto &v : edges_in[i]) {
                 int y = comp[v];
                 if (x == y)
                     continue;
-                ret[x].push_back(y);
+                dag[x].push_back(y);
             }
         }
-        return ret;
     }
 };
--- library/tree/lca.cpp
+++ library/tree/lca.cpp
@@ -19,6 +19,11 @@ struct LCA {
     LCA(int n)
         : n(n), k(0), depth(2 * n - 1), eulerTour(2 * n - 1), firstVisit(n), edges(n),
           segTree(2 * n, pairComp, make_pair(numeric_limits<int>::max(), 0)), isbuilt(false){};
+    LCA(vector<vector<int>> &edges, int root = 0)
+        : n(edges.size()), k(0), depth(2 * n - 1), eulerTour(2 * n - 1), firstVisit(n), edges(edges),
+          segTree(2 * n, pairComp, make_pair(numeric_limits<int>::max(), 0)), isbuilt(false) {
+        build(root);
+    };
     void addEdge(int from, int to) {
         edges[from].push_back(to);
         edges[to].push_back(from);
@@ -52,4 +57,5 @@ struct LCA {
         return segTree.query(left, right + 1).second;
     }
     int getDepth(int k) { return depth[firstVisit[k]]; }
+    int dist(int l, int r) { return getDepth(l) + getDepth(r) - 2 * getDepth(operator()(l, r)); }
 };
